f86e6051d8a745a548b60b01b44a9f77a9ea3d3f
enricoru <enricoru@users.noreply.github.com>
2024-05-15 18:17:29 +0200

    Create lancia.js

---
+++ tuttifrutti/lancia.js
@@ -0,0 +1,15 @@
+const trackBtn = document.getElementById('track-btn');
+
+trackBtn.onclick = () => {
+
+    console.log('trackBtn pressetd');
+
+    if (window.Worker) {
+        console.log('Message posted to worker');
+        const myWorker = new Worker(".\dove.js");
+     
+
+    } else {
+        console.log('Your browser doesn\'t support web workers.');
+    }
+}
