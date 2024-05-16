8bdade0ff4d9cd5a5d65009e1d5a8a99eb260723
enricoru <enricoru@users.noreply.github.com>
2024-05-16 14:24:43 +0200

    Update worker.js

--- tuttifrutti/worker.js
+++ tuttifrutti/worker.js
@@ -1,6 +1,5 @@
 onmessage = function(e) {
   console.log('Worker: Message received from main script');
-
   var interval = e * 1000;
   if (interval > 0) {
     currentData = [];
@@ -8,18 +7,9 @@ onmessage = function(e) {
       collectEntry();
       position = currentData.slice(-10).map(JSON.stringify).reverse().join(',\n');
     }, interval);
-  
+    }  
   isTracking = true;
 
   postMessage(position);
-  
-  //console.log(e);
- /* const result = e.data[0] * e.data[1];
-  if (isNaN(result)) {
-    postMessage('Please write two numbers');
-  } else {
-    const workerResult = 'Result: ' + result;
-    console.log('Worker: Posting message back to main script');
-    postMessage(workerResult);
-  }*/
-}
+}  
+
