05ea29929381515d9422c0e164bb5cb2d669a196
enricoru <enricoru@users.noreply.github.com>
2024-05-16 11:00:39 +0200

    Update worker.js

--- tuttifrutti/worker.js
+++ tuttifrutti/worker.js
@@ -1,6 +1,6 @@
 onmessage = function(e) {
   console.log('Worker: Message received from main script');
-  console.log(e);
+  //console.log(e);
  /* const result = e.data[0] * e.data[1];
   if (isNaN(result)) {
     postMessage('Please write two numbers');
