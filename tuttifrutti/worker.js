edb24965418399ac38b90b6765bc1aab929702db
enricoru <enricoru@users.noreply.github.com>
2024-05-16 14:06:13 +0200

    Update worker.js

--- tuttifrutti/worker.js
+++ tuttifrutti/worker.js
@@ -1,6 +1,17 @@
 onmessage = function(e) {
   console.log('Worker: Message received from main script');
-  postMessage('message from worker');
+
+  var interval = e * 1000;
+  if (interval > 0) {
+    currentData = [];
+    trackingIntervalId = window.setInterval(() => {
+      collectEntry();
+      position = currentData.slice(-10).map(JSON.stringify).reverse().join(',\n');
+    }, interval);
+  }
+  isTracking = true;
+}
+  postMessage(position);
   
   //console.log(e);
  /* const result = e.data[0] * e.data[1];
